0477d34dd151836c48b5cff4acdce961558f0df6
Sean Wu <seanwu20@gmail.com>
2019-08-19 23:00:41 -0400

    added route

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const server = express();
+const db = require('./data/dbConfig');
+
 
 const middleWareConfig = require("./config/middleware.js");
 
 middleWareConfig(server);
 
 server.get("/", (req, res) => {
-    res.status(200).json({ api: "up and running!" });
+    db('users')
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
 });
 
 module.exports = server;
