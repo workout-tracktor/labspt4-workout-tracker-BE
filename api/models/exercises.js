6fded8214001261e524cd893d7b806d91f4e7d27
Dustin Awesome <afrobacon@gmail.com>
2019-09-19 00:39:21 -0400

    small update

--- api/models/exercises.js
+++ api/models/exercises.js
@@ -2,33 +2,46 @@
 const db = require('../../data/dbConfig')
 
 //VARIABLES
-const db_dame = 'exercises'
+const db_name = 'exercises'
 
 //MODELS
 //create
 //:add a new exercise
-const add = async exercise => await db(db_dame).insert(exercise)
+const add = async exercise => {
+    try {
+        await db(db_name).insert(exercise)
+        // await db.raw(`INSERT INTO exercises (name, exercise_id, equipment_id) VALUES ('bob2', 'three', 2)`)
+        // await db(db_name).insert({
+        //     exercise_id: 'one2',
+        //     name: 'test2',
+        //     equipment_id: db(db_name).raw([1,2,3])
+        // })
+    } catch(err) {
+        console.log(err)
+    }
+    return await db(db_name).where({name: exercise.name})
+}
 
 //read
 //:returns an array of all exercises
-const get_all = async () => await db(db_dame)
+const get_all = async () => await db(db_name)
 
 //returns an array of exercises fitting a set of requirements
-const get_all_by = async obj => await db(db_dame).where(obj)
+const get_all_by = async obj => await db(db_name).where(obj)
 
 //:returns a single exercise fitting a set of  requirements
-const get_by = async obj => await db(db_dame).where(obj).first()
+const get_by = async obj => await db(db_name).where(obj).first()
 
 //update
 //:updates a exercise with a specific id
 const update_by_id = async (id, obj) => {
-    await db(db_dame).where({id: id}).update(obj)
+    await db(db_name).where({id: id}).update(obj)
     return await get_by({id: id})
 }
 
 //delete
 //:terminates a exercise
-const remove_by_id = async (id) => await db(db_dame).where({id: id}).delete()
+const remove_by_id = async (id) => await db(db_name).where({id: id}).delete()
 
 //EXPORT
 module.exports = {
