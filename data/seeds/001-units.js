72ebc0e89fa2a6eb2de3202b315fc8a781a9be85
Mychal <hallmychal@gmail.com>
2019-09-26 18:35:37 -0500

    'this'

--- data/seeds/001-units.js
+++ data/seeds/001-units.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("units")
-    .del()
+
     .then(function() {
       // Inserts seed entries
       return knex("units").insert([
