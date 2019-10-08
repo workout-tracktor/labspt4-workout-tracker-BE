387ff2ce1413955ec291ce745db86fe072c8b106
unknown <afrobacon@gmail.com>
2019-10-07 22:38:54 -0400

    required and (maybe) unique fields for post request are set up and working

--- api/middleware/constraints.js
+++ api/middleware/constraints.js
@@ -1,7 +1,7 @@
 /*
 
 
-What is do?
+What it do?
 Checks to see if post requests have both required and unique fields.
 Checks put and delete for id field
 */
@@ -9,29 +9,38 @@ Checks put and delete for id field
 const get = require('../helpers/get')
 const check = require('../helpers/check')
 const {unqiue_fields} = require('../../config/unique_fields')
+const {send_error} = require('../helpers/errors')
 
-const post_requirements = async (table, body) => {
-    //check if all required fields are provided
+const requirements = async (table, body) => {
     const not_required = ['created', 'updated']
     const required_fields = (await get.required(table)).filter(field => not_required.indexOf(field) === -1)
     const missing_fields = required_fields.filter(field => !body.hasOwnProperty(field))
-    if(missing_fields.length > 0) console.log('send error', missing_fields)
+    return {required_fields: required_fields, missing_fields: missing_fields}
+}
 
+const unique = async (table, body) => {
     const unique_fields = unqiue_fields[table]
     const unremarkable_fields = (await check.unique(table, body, unique_fields))
-    console.log(unremarkable_fields)
-
-    
-    // console.log('req', required_fields, unique_fields)
+    return {unique_fields: unique_fields, unremarkable_fields: unremarkable_fields}
 }
 
 const constraints = async (req, res, next) => {
     switch(req.method) {
-        case 'POST': post_requirements(req.data.table, req.body); break
+        case 'POST': {
+            //check if all required fields are present
+            const {required_fields, missing_fields} = await requirements(req.data.table, req.body)
+            if(missing_fields.length) return send_error(res, '23502', req.data.table, missing_fields, required_fields)
+
+            //check if all unique fields are in fact unique
+            const {unique_fields, unremarkable_fields} = await unique(req.data.table, req.body)
+            if(unique_fields.length) return send_error(res, '23505', req.data.table, unique_fields, unremarkable_fields)
+            break
+        }
         case 'GET': next(); break
         case 'PUT': console.log('PUT'); break
         case 'DELETE': console.log('DELETE'); break
     }
+    console.log('made it here')
     next()
 }
 
