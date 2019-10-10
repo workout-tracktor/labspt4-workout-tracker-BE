2e54bc39f3da40df3df8f2d978f8b537c831e156
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-10-09 18:45:45 -0700

    Get user by username test

--- __tests__/users.spec.js
+++ __tests__/users.spec.js
@@ -53,7 +53,7 @@ describe('GET /users', () => {
 
 });
 
-describe('getUserById', () => {
+describe('get user by filter', () => {
     // cleanup for db
     beforeEach(done => {
         knex.migrate.rollback()
@@ -92,6 +92,51 @@ describe('getUserById', () => {
 
         const res = await request(server).get(`/api/user?user_id=${user[0].user_id}`);
 
+        expect(res.status).toBe(200);
         expect(res.body.username).toEqual("Msmith9");
+    });
+});
+
+describe('getUserByUsername', () => {
+    // cleanup for db
+    beforeEach(done => {
+        knex.migrate.rollback()
+          .then(() => {
+            knex.migrate.latest()
+            .then(() => {
+              done()
+            })
+          }).catch(err => done(err))
+    });
+
+    it('finds a user by username', async () => {
+
+        const user = [
+            {
+                id: 1,
+                user_id: "1",
+                username: "Msmith9",
+                password: "test",
+                email: "smith5w@gmail.com",
+                created: "1570585236381",
+                updated: "1570585236381"
+            },
+            {
+                id: 2,
+                user_id: "2",
+                username: "lijones",
+                password: "test",
+                email: "jones@gmail.com",
+                created: "1570585236381",
+                updated: "1570585236381"
+            }
+        ];
+
+        await db('users').insert(user);
+
+        const res = await request(server).get(`/api/user?username=${user[0].username}`);
+
+        expect(res.status).toBe(200);
+        expect(res.body.email).toEqual("smith5w@gmail.com");
     });
 });
